7508ff10ae30635578fa2feaa57a0f701faf795d
Hadi Nategh <hnategh@gmail.com>
2020-10-12 18:13:47 +0200

    Add missing js file for overlay interface

---
+++ js/et2_videooverlay_interface.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var PlayerMode;
+(function (PlayerMode) {
+    PlayerMode[PlayerMode["Unchanged"] = 0] = "Unchanged";
+    PlayerMode[PlayerMode["Pause"] = 1] = "Pause";
+    PlayerMode[PlayerMode["Disable"] = 2] = "Disable";
+})(PlayerMode = exports.PlayerMode || (exports.PlayerMode = {}));
+var et2_IOverlayElement = "et2_IOverlayElement";
+function implements_et2_IOverlayElement(obj) {
+    return implements_methods(obj, ["keepRunning"]);
+}
+var et2_IOverlayElementEditor = "et2_IOverlayElementEditor";
+function implements_et2_IOverlayElementEditor(obj) {
+    return implements_methods(obj, ["onSaveCallback"]);
+}
+//# sourceMappingURL=et2_videooverlay_interface.js.map
