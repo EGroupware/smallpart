ae01658edf3b78ccb4ccf12a77e6ee5e068f53f9
Hadi Nategh <hnategh@gmail.com>
2021-06-30 09:53:24 +0200

    * SmallPart: Fix overlay slider controller does not get proper size after window being resized

--- js/et2_widget_videobar.js
+++ js/et2_widget_videobar.js
@@ -349,8 +349,8 @@ var et2_smallpart_videobar = /** @class */ (function (_super) {
         //redraw marks and tags to get the right ratio
         this.setMarks(this.getMarks());
         this.set_slider_tags(this.comments);
-        if (typeof this.options.onresize_callback == 'function') {
-            this.options.onresize_callback.call(this, this.video.width(), this.video.height(), this._vtimeToSliderPosition(this.currentTime()));
+        if (typeof this.onresize_callback == 'function') {
+            this.onresize_callback.call(this, this.video.width(), this.video.height(), this._vtimeToSliderPosition(this.currentTime()));
         }
     };
     /**
