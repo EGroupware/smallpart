7f9706af2b2a1f4397c0a9d76ec7528d605e37a0
hadi <hnategh@gmail.com>
2023-06-15 16:29:55 +0200

    WIP converting comment timespan widget to webcomponent - Fix duration widgets - Fix picker button not working

--- js/SmallPartCommentTimespan.ts
+++ js/SmallPartCommentTimespan.ts
@@ -9,16 +9,16 @@
  */
 
 import {css, html, LitElement, TemplateResult, until} from "@lion/core";
-import {Et2InputWidget} from "../../api/js/etemplate/Et2InputWidget/Et2InputWidget";
 import {et2_smallpart_videobar} from "./et2_widget_videobar";
 import {Et2DateDuration} from "../../api/js/etemplate/Et2Date/Et2DateDuration";
 import {Et2Button} from "../../api/js/etemplate/Et2Button/Et2Button";
+import {Et2Widget} from "../../api/js/etemplate/Et2Widget/Et2Widget";
 
 /**
  *
  *
  */
-export class SmallPartCommentTimespan extends Et2InputWidget(LitElement)
+export class SmallPartCommentTimespan extends Et2Widget(LitElement)
 {
 	protected widgets : {
 		starttime: Et2DateDuration,
@@ -26,6 +26,8 @@ export class SmallPartCommentTimespan extends Et2InputWidget(LitElement)
 		starttimePicker: Et2Button,
 		stoptimePicker: Et2Button
 	} = {starttime:null, stoptime:null, starttimePicker:null, stoptimePicker: null};
+
+	protected _videobar: et2_smallpart_videobar;
 
 	static get styles()
 	{
@@ -68,29 +70,57 @@ export class SmallPartCommentTimespan extends Et2InputWidget(LitElement)
 
 	}
 
-	public render() : TemplateResult
+	/**
+	 *
+	 */
+	firstUpdated()
 	{
-		// This shows loading template until loadingPromise resolves, then shows _listTemplate
-		return html`
-            <div>
-                <et2-date-duration displayFormat="hms" dataFormat="s" class="starttime"></et2-date-duration>
-				<et2-button statustext="start-time picker" class="starttime glyphicon glyphicon-pushpin"></et2-button>
-                <et2-date-duration displayFormat="hms" dataFormat="s" class="stoptime"></et2-date-duration>
-                <et2-button statustext="stop-time picker" class="stoptime glyphicon glyphicon-pushpin"></et2-button>
-            </div>
-		`;
-	}
-
-	public connectedCallback()
-	{
-		super.connectedCallback();
+		super.firstUpdated();
+		//the dom is ready get the widgets
 		this.widgets = {
 			starttime: this.getStarttime(),
 			starttimePicker: this.getStarttimePicker(),
 			stoptime: this.getStoptime(),
 			stoptimePicker: this.getStoptimePicker()
 		};
+		this.widgets.starttime.value = this.options.starttime;
+		this.widgets.stoptime.value = this.options.stoptime;
+	}
 
+	public render() : TemplateResult
+	{
+		// This shows loading template until loadingPromise resolves, then shows _listTemplate
+		return html`
+            <et2-hbox>
+                <et2-date-duration 
+						displayFormat="hms" 
+						dataFormat="s" 
+						class="starttime"
+						.selectUnit=${false}>
+				</et2-date-duration>
+				<et2-button-icon
+						statustext="start-time picker"
+						class="starttime"
+                        .noSubmit=${true}
+						image="align-start"
+						@click=${this._timePicker.bind(this, 'starttime')}>
+				</et2-button-icon>
+                <et2-date-duration 
+						displayFormat="hms"
+						dataFormat="s" 
+						class="stoptime" 
+						.selectUnit=${false}
+						@change=${this._checkTimeConflicts}
+				></et2-date-duration>
+                <et2-button-icon 
+						statustext="stop-time picker"
+						class="stoptime"
+                        .noSubmit=${true}
+                        image="align-end"
+						@click=${this._timePicker.bind(this, 'stoptime')}
+				></et2-button-icon>
+            </et2-hbox>
+		`;
 	}
 
 	/**
@@ -132,6 +162,16 @@ export class SmallPartCommentTimespan extends Et2InputWidget(LitElement)
 	 */
 	set videobar(_id_or_widget: string|et2_smallpart_videobar)
 	{
+		if (typeof _id_or_widget === 'string') {
+			_id_or_widget = <et2_smallpart_videobar>this.getRoot().getWidgetById(_id_or_widget);
+		}
+		if (typeof _id_or_widget !== 'string' && _id_or_widget ) {
+			this._videobar = _id_or_widget;
+			this._videobar.video[0].addEventListener("et2_video.onReady." + this._videobar.id, _ => {
+				this._set_widgets();
+			});
+		}
+
 	}
 
 
@@ -153,19 +193,20 @@ export class SmallPartCommentTimespan extends Et2InputWidget(LitElement)
 
 	/**
 	 * time picker button click handler
-	 * @param _node
-	 * @param _widget
+	 * @param _type
+	 * @param _event
 	 * @private
 	 */
-	private _timePicker(_node, _widget)
+	private _timePicker(_type, _event)
 	{
-		if (_widget == this.widgets.starttimePicker)
+		debugger;
+		if (_type == 'starttime')
 		{
-			this.widgets.starttime.value = Math.round(this.videobar.currentTime()).toString();
+			this.widgets.starttime.value = Math.round(this._videobar.currentTime()).toString();
 		}
 		else
 		{
-			this.widgets.stoptime.value = Math.round(this.videobar.currentTime()).toString();
+			this.widgets.stoptime.value = Math.round(this._videobar.currentTime()).toString();
 		}
 	}
 }
