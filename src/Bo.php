ef0a641cb47d8e96a71dd972dbeba6624a2eb8de
hadi <hnategh@gmail.com>
2023-10-18 15:01:59 +0200

    Set duration time for webm files based on provided duration url param

--- src/Bo.php
+++ src/Bo.php
@@ -320,6 +320,14 @@ class Bo
 					}
 				}
 			}
+
+			if ($video['video_type'] == 'webm' && $lf['session_endtime'] && $lf['session_starttime'])
+			{
+				// webm video has issues with providing duration because browser needs to load the whole file before being able to
+				// show its duration. In order to tackle this issue we just calculate the duration time base on session time and send it
+				// via duration url param to be proccessed in client-side video widget
+				$video['video_src'] = $video['video_src'].'&duration='. (Api\DateTime::to($lf['session_endtime'], 'ts') - Api\DateTime::to($lf['session_starttime'], 'ts'));
+			}
 		}
 		return $videos;
 	}
