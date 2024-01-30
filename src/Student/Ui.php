EGroupware/smallpart

commit 32dfb033d51d6586bc0718b1284971d8070d381f

Feedback is always to take 1s
Fixed from 5a6eb60 when duration was incorrectly changed to take as long as it took to comment

src/Student/Ui.php
@@ -1090,9 +1090,8 @@ class Ui
 			if ($record && empty($record['session_endtime']) && !empty($record['session_starttime']))
 			{
 				$now = new Api\DateTime('now');
-				$comment_time = $now->getTimestamp() - Api\DateTime::to($record['session_starttime'], 'ts');
-				$comment['comment_starttime'] = $comment['comment_starttime'] ? intval($comment['comment_starttime']) : $comment_time;
-				$comment['comment_stoptime'] = $comment_time + 1;
+				$comment['comment_starttime'] = $now->getTimestamp() - Api\DateTime::to($record['session_starttime'], 'ts');
+				$comment['comment_stoptime'] = $comment['comment_starttime'] + 1;
 				self::ajax_saveComment($exec_id, $comment);
 			}
 			else if($record && empty($record['session_endtime']) && empty($record['session_starttime']))

commit 2e22d89bf320523e2706b5e02e76018183aa2051

Fix teacher feedback timing again start time offset broken by 32dfb03

src/Student/Ui.php
@@ -1090,7 +1090,7 @@ class Ui
 			if ($record && empty($record['session_endtime']) && !empty($record['session_starttime']))
 			{
 				$now = new Api\DateTime('now');
-				$comment['comment_starttime'] = $now->getTimestamp() - Api\DateTime::to($record['session_starttime'], 'ts');
+				$comment['comment_starttime'] = $comment['comment_starttime'] ? intval($comment['comment_starttime']) : $now->getTimestamp() - Api\DateTime::to($record['session_starttime'], 'ts');
 				$comment['comment_stoptime'] = $comment['comment_starttime'] + 1;
 				self::ajax_saveComment($exec_id, $comment);
 			}