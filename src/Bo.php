EGroupware/smallpart

commit 692f7cc5a097214fcfa6fe8c19c0bec9e9d5823c

Fix categories were not imported properly

src/Bo.php
@@ -2266,20 +2266,16 @@ class Bo
 			$cat_ids = [];
 			foreach($keys['cats'] as $key => &$cat)
 			{
-				$cat = [
-					'course_id' => $course['course_id'],
-					'cat_name'  => $cat['cat_name'],
-					'cat_acronym' => $cat['cat_acronym'],
-					'cat_description' => $cat['cat_description'],
-					'cat_color' => $cat['cat_color'],
-				]+(array)json_decode($cat['data'], true);
-				$cat['parent_id'] = !empty($cat['parent_id']) && isset($cat_ids[$cat['parent_id']]) ? $cat_ids[$cat['parent_id']] : null;
-				$cat['cat_id'] = $cat_ids[$cat['cat_id']] = $this->so->updateCategory($cat);
+				$cat['course_id'] = $course['course_id'];
+				$cat += (array)json_decode($cat['data'], true);
+				$cat['parent_id'] = !empty($cat['parent_id']) && in_array($cat['parent_id'], $cat_ids) ? $cat['parent_id'] : null;
+				$cat['cat_id'] = $cat_ids[] = $this->so->updateCategory($cat);
 				// encode the newly generated value back into data
 				$cat['data'] = json_encode($cat);
 			}
 			$this->so->deleteCategories($course['course_id'], $cat_ids, true);
-			$course['cats'] = [0=>false]+$keys['cats'];
+			array_unshift($keys['cats'], false);
+			$course['cats'] = $keys['cats'];
 		}
 
 		// push course updates to participants (new course are ignored for now)

src/Export.php
@@ -61,6 +61,9 @@ class Export
 		{
 			throw new Api\Exception\WrongUserinput(lang('Error decoding JSON file!'));
 		}
+		// Don't save cats unless provided in the file
+		unset($course['cats']);
+
 		if ($overwrite && $course_id)
 		{
 			if (!is_array($course) && !($course = $this->bo->read($course)))
@@ -89,6 +92,7 @@ class Export
 				$course['participants'] = [];
 			}
 		}
+
 		foreach($json['videos'] as $video)
 		{
 			foreach(So::$video_timestamps as $name)
@@ -105,7 +109,44 @@ class Export
 
 			$course['videos'][] = $video;
 		}
+
+		// Need to clear cat_id or it won't be saved, need to keep it for hierarchy
+		$cat_id_index = [];
+		$parent_id_index = [];
+		foreach($course['cats'] as $index => &$cat)
+		{
+			$cat_id_index[$cat['cat_id']] = count($cat_id_index);
+			$parent_id_index[] = $cat_id_index[$cat['parent_id']] ?? null;
+			unset($cat['cat_id']);
+		}
 		$course = $this->bo->save($course);
+
+		// Fix cat hierarchy - saving strips the IDs, loses parent and re-orders the categories
+		$need_cat_save = false;
+		$cat_id_index = [];
+		if($course['cats'][0] == false)
+		{
+			unset($course['cats'][0]);
+		}
+		foreach($course['cats'] as $index => &$cat)
+		{
+			if(!isset($cat['cat_id']))
+			{
+				continue;
+			}
+			$parent_id = $cat_id_index[$parent_id_index[count($cat_id_index)]] ?? null;
+			$cat_id_index[] = $cat['cat_id'];
+			if($parent_id !== $cat['parent_id'])
+			{
+				$cat['parent_id'] = $parent_id;
+				$need_cat_save = true;
+			}
+		}
+		if($need_cat_save)
+		{
+			$course = $this->bo->save($course);
+		}
+
 
 		// bo::save only subscribes owner for new courses, but overwrite unsubscribes everyone --> subscribe owner (again)
 		if ($overwrite && $course_id)
@@ -119,6 +160,10 @@ class Export
 			// find new video_id (search in reverse order, as new videos are added at the end, in case same video is imported)
 			foreach(array_reverse($course['videos']) as $video_new)
 			{
+				if(!$video_new)
+				{
+					continue;
+				}
 				if ($video['video_url'] === $video_new['video_url'] &&
 					$video['video_name'] === $video_new['video_name'] &&
 					$video['video_question'] === $video_new['video_question'])
@@ -200,6 +245,13 @@ class Export
 					$video['max_score'] = (float)$overlay['max_score'];
 				}
 			}
+		}
+		// Re-pack cat data
+		foreach($course['cats'] as &$cat)
+		{
+			$cat['data'] = json_encode(array_diff_key($cat, array_flip(['cat_id', 'course_id', 'parent_id', 'cat_name',
+																		'cat_description', 'cat_color'])),
+									   JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
 		}
 		$json = json_encode($course, JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
 		if (function_exists('bzcompress'))

src/So.php
@@ -770,7 +770,9 @@ class So extends Api\Storage\Base
 	{
 		if ($_data['course_id'] && !empty($_data['cat_name']))
 		{
-			$_data['cat_data'] = json_encode(array_diff_key($_data, array_flip(['cat_id','course_id','parent_id','cat_name','cat_description','cat_color'])),
+			$_data['cat_data'] = json_encode(array_diff_key($_data, array_flip(['cat_id', 'course_id', 'parent_id',
+																				'cat_name', 'cat_description',
+																				'cat_color', 'cat_data', 'data'])),
 				JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES);
 			if (empty($_data['cat_id']) || !is_numeric($_data['cat_id']))
 			{

commit 40b060e994f1f1c4175aba1e713202aca318616c

Fix regular category save through UI was broken by 692f7cc

src/Bo.php
@@ -2268,8 +2268,18 @@ class Bo
 			{
 				$cat['course_id'] = $course['course_id'];
 				$cat += (array)json_decode($cat['data'], true);
-				$cat['parent_id'] = !empty($cat['parent_id']) && in_array($cat['parent_id'], $cat_ids) ? $cat['parent_id'] : null;
-				$cat['cat_id'] = $cat_ids[] = $this->so->updateCategory($cat);
+				$original_cat_id = $cat['cat_id'];
+				$cat['parent_id'] = !empty($cat['parent_id']) && isset($cat_ids[$cat['parent_id']]) ? $cat_ids[$cat['parent_id']] : null;
+				$cat['cat_id'] = $this->so->updateCategory($cat);
+				if($original_cat_id)
+				{
+					$cat_ids[$original_cat_id] = $cat['cat_id'];
+				}
+				else
+				{
+					// Adding a new cat, don't delete it in deleteCategories
+					$cat_ids[] = $cat['cat_id'];
+				}
 				// encode the newly generated value back into data
 				$cat['data'] = json_encode($cat);
 			}

src/Export.php
@@ -110,18 +110,20 @@ class Export
 			$course['videos'][] = $video;
 		}
 
-		// Need to clear cat_id or it won't be saved, need to keep it for hierarchy
-		$cat_id_index = [];
-		$parent_id_index = [];
+		// Need to change cat_id or it won't be saved properly
 		foreach($course['cats'] as $index => &$cat)
 		{
-			$cat_id_index[$cat['cat_id']] = count($cat_id_index);
-			$parent_id_index[] = $cat_id_index[$cat['parent_id']] ?? null;
-			unset($cat['cat_id']);
+			$cat['cat_id'] .= '_imported';
+			if($cat['parent_id'])
+			{
+				$cat['parent_id'] .= '_imported';
+			}
+
 		}
 		$course = $this->bo->save($course);
 
 		// Fix cat hierarchy - saving strips the IDs, loses parent and re-orders the categories
+		/*
 		$need_cat_save = false;
 		$cat_id_index = [];
 		if($course['cats'][0] == false)
@@ -146,6 +148,7 @@ class Export
 		{
 			$course = $this->bo->save($course);
 		}
+		*/
 
 
 		// bo::save only subscribes owner for new courses, but overwrite unsubscribes everyone --> subscribe owner (again)