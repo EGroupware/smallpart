37cf910a8183f4bebe2c497ed74a5fa3156687c8
ralf <rb@egroupware.org>
2024-08-18 10:04:13 +0200

    fixed not displayed favorite question in FireFox caused by "&nbsp;" entity also including favorite in overlay-controller so it would work there too

--- js/et2_widget_videooverlay.ts
+++ js/et2_widget_videooverlay.ts
@@ -25,6 +25,7 @@ import "./overlay_plugins/et2_smallpart_overlay_html";
 import "./overlay_plugins/et2_smallpart_question_multiplechoice";
 import "./overlay_plugins/et2_smallpart_question_singlechoice";
 import "./overlay_plugins/et2_smallpart_question_rating";
+import "./overlay_plugins/et2_smallpart_question_favorite";
 import "./overlay_plugins/et2_smallpart_question_markchoice";
 import "./overlay_plugins/et2_smallpart_question_millout";
 import "./overlay_plugins/et2_smallpart_question_text";
