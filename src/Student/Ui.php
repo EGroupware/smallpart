8de3d9a167cfd3b53a3cc460a1c1e7288a2c90b6
nathan <nathangray.bsc+github@gmail.com>
2024-11-07 10:58:10 -0700

    Let video area take full width for livestreamer

--- src/Student/Ui.php
+++ src/Student/Ui.php
@@ -556,6 +556,12 @@ class Ui
 			});
 			$content['legend_cats'] = $content['cats'];
 			array_unshift($content['legend_cats'], false);
+
+			// Set class for position
+			$content['video']['area_class'] = $content['video']['is_admin'] && in_array(
+				$content['video']['livefeedback_session'], ['not-started', 'running']
+			) ?
+				'et2-layout-full-span' : '';
 		}
 
 		// if we display all questions as list, we need to send them to the client-side
