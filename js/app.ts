EGroupware/smallpart

commit d0d0ba873b04b0284a2debc5166a18d06337bae1

Fix category did not hide 8b847ba91aacc20ed8e7c304ba460ed1ac93daaf

js/app.ts
@@ -1755,7 +1755,7 @@ export class smallpartApp extends EgwApp
 			comment_cat: 'free'
 		});
 
-		comment.set_value({content: this.edited});
+		comment.set_value({content: {...this.edited, ...comment.getArrayMgr("content").data}});
 		comment.getWidgetById('deleteComment').set_disabled(true);
 		this._student_controlCommentAreaButtons(true);
 		comments_slider?.disableCallback(true);

commit 881563bc01890af04184a46f7af1f7b20f146643

Fix comment did not take current video time
Broken by d0d0ba873b04b0284a2debc5166a18d06337bae1
Comment time was correct the first time, but subsequent comments were always for the same time.

js/app.ts
@@ -1752,7 +1752,6 @@ export class smallpartApp extends EgwApp
 		const content_cats = this.et2.getArrayMgr('content').getEntry('cats');
 		this.edited = jQuery.extend(this.student_getFilter(), {
 			account_lid: this.egw.user('account_lid'),
-			comment_starttime: Math.round(videobar.currentTime()),
 			comment_added: [''],
 			comment_color: smallpartApp.default_color,
 			action: 'edit',
@@ -1761,7 +1760,13 @@ export class smallpartApp extends EgwApp
 			comment_cat: 'free'
 		});
 
-		comment.set_value({content: {...this.edited, ...comment.getArrayMgr("content").data}});
+		comment.set_value({
+			content: {
+				...this.edited,
+				...comment.getArrayMgr("content").data,
+				comment_starttime: Math.round(videobar.currentTime())
+			}
+		});
 		comment.getWidgetById('deleteComment').set_disabled(true);
 		this._student_controlCommentAreaButtons(true);
 		comments_slider?.disableCallback(true);