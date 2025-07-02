EGroupware/smallpart

commit 07756069149196ffde191e706677ded4a44d990d

Fix add comment sub-category select did not get enabled when a parent category was selected

js/app.ts
@@ -3710,7 +3710,7 @@ export class smallpartApp extends EgwApp
 
 	public student_commentCatChanged(_ev, _widget)
 	{
-		let commentCatSub = this.et2.getWidgetById('comment_cat_sub');
+		let commentCatSub = _widget.getParent().getWidgetById('comment_cat_sub');
 		if (commentCatSub)
 		{
 			commentCatSub.disabled = _widget.value.trim() == "free";

commit da96da8fc8b59fc796e3bb4726738a5651ff1590

Only enable subcategory for edit (not reply)
broken by 07756069149196ffde191e706677ded4a44d990d

js/app.ts
@@ -1062,9 +1062,9 @@ export class smallpartApp extends EgwApp
 					if(_action.id == 'edit')
 					{
 						this.edited["comment_added"] = undefined;
+						this.student_commentCatChanged(null, comment.getWidgetById("comment_cat"));
 					}
 					comment.set_value({content: this.edited});
-					this.student_commentCatChanged(null, comment.getWidgetById("comment_cat"));
 					comments_slider?.disableCallback(true);
 					videooverlay.getElementSlider().disableCallback(true);
 					break;
@@ -1090,8 +1090,8 @@ export class smallpartApp extends EgwApp
 						action: action,
 						video_duration: videobar.duration()
 					}});
-					this.et2.getWidgetById('comment_editBtn')?.set_disabled(!(this.is_staff || this.edited.account_id == egw.user('account_id')) || accessible === 'readonly');
-					this.et2.getWidgetById("comment_added").editable = (this.is_staff || this.edited.account_id == egw.user('account_id'));
+					comment.getWidgetById('comment_editBtn')?.set_disabled(!(this.is_staff || this.edited.account_id == egw.user('account_id')) || accessible === 'readonly');
+					comment.getWidgetById("comment_added").editable = (this.is_staff || this.edited.account_id == egw.user('account_id'));
 					if (comments_slider)
 					{
 						comments_slider.disableCallback(false);